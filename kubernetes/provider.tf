a99c88fcea866252dead2833d5d1b07bfcf1347c
Jono Cooper <jono@jonocooper.com>
2019-12-04 00:08:27 +1300

    Add Tiller Service Account

--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -7,7 +7,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  service_account = "tiller"
+  service_account = kubernetes_service_account.tiller.metadata[0].name
   kubernetes {
     host  = digitalocean_kubernetes_cluster.sfo2.endpoint
     token = digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
