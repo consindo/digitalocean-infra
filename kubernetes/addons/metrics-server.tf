11da7c92407506c09a027e8ce5cfb01b01e999d5
Jono Cooper <jono@jonocooper.com>
2019-12-04 00:28:08 +1300

    Add Metrics Server Config for Digital Ocean

--- kubernetes/addons/metrics-server.tf
+++ kubernetes/addons/metrics-server.tf
@@ -4,4 +4,17 @@ resource "helm_release" "metrics-server" {
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "metrics-server"
   version    = "2.8.8"
+
+  values = [
+    local.metrics-server-values
+  ]
+}
+
+locals {
+  metrics-server-values = <<EOT
+args:
+- --logtostderr
+- --kubelet-preferred-address-types=InternalIP
+- --kubelet-insecure-tls
+  EOT
 }
