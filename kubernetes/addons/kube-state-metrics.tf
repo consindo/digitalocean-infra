c18cc5addc140f7339d309831daef9ced362e983
Jono Cooper <jono@jonocooper.com>
2019-12-04 00:21:48 +1300

    Change Namespace

--- kubernetes/addons/kube-state-metrics.tf
+++ kubernetes/addons/kube-state-metrics.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "kube-state-metrics" {
   name       = "kube-state-metrics"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = "kube-system"
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "kube-state-metrics"
   version    = "2.4.1"
--- kubernetes/addons/metrics-server.tf
+++ kubernetes/addons/metrics-server.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "metrics-server" {
   name       = "metrics-server"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = "kube-system"
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "metrics-server"
   version    = "2.8.8"
