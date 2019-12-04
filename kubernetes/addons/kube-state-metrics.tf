02c8bf3a95ccb4a33e824955d10f0f30afeb7169
Jono Cooper <jono@jonocooper.com>
2019-12-04 20:20:13 +1300

    Move Namespace of Metrics Server

--- kubernetes/addons/kube-state-metrics.tf
+++ kubernetes/addons/kube-state-metrics.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "kube-state-metrics" {
   name       = "kube-state-metrics"
-  namespace  = "kube-system"
+  namespace  = "kube-system" # has to go in here, otherwise the DigitalOcean UI doesn't pick up metrics
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "kube-state-metrics"
   version    = "2.4.1"
--- kubernetes/addons/metrics-server.tf
+++ kubernetes/addons/metrics-server.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "metrics-server" {
   name       = "metrics-server"
-  namespace  = "kube-system"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "metrics-server"
   version    = "2.8.8"
