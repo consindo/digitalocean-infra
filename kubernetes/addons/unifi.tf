c9e6435359c65277e664728acfd808d50a0bebce
Jono Cooper <jono.cooper@xero.com>
2019-12-31 00:34:03 +1300

    Add Ingress

--- kubernetes/addons/unifi.tf
+++ kubernetes/addons/unifi.tf
@@ -4,4 +4,19 @@ resource "helm_release" "unifi" {
   repository = data.helm_repository.stable.metadata[0].name
   chart      = "unifi"
   version    = "0.5.2"
+
+  values = [
+    local.unifi-values
+  ]
+}
+
+locals {
+  unifi-values = <<EOT
+ingress:
+  enabled: true
+  annotations:
+    kubernetes.io/ingress.class: nginx
+  hosts:
+    - jono-home.dymajo.com
+  EOT
 }
