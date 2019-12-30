dec8c3c52891ae7b0bcaeaa642be1603072950ec
Jono Cooper <jono.cooper@xero.com>
2019-12-31 10:58:22 +1300

    Change Node Pool Configuration & Upgrade Cluster

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -1,13 +1,21 @@
 resource "digitalocean_kubernetes_cluster" "sfo2" {
   name    = "sfo2-prod"
   region  = "sfo2"
-  version = "1.16.2-do.0"
+  version = "1.16.2-do.1"
 
   node_pool {
     name       = "memory-optimized"
     size       = "s-1vcpu-3gb"
     auto_scale = true
-    min_nodes  = 2
-    max_nodes  = 5
+    min_nodes  = 1
+    max_nodes  = 3
+  }
+
+  node_pool {
+    name       = "general-purpose"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 3
   }
 }
