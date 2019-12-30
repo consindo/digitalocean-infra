37612b9a4526477e2e41cb74e1d6682179892dc4
Jono Cooper <jono.cooper@xero.com>
2019-12-31 11:01:02 +1300

    Change Block to Resource

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -10,12 +10,14 @@ resource "digitalocean_kubernetes_cluster" "sfo2" {
     min_nodes  = 1
     max_nodes  = 3
   }
-
-  node_pool {
-    name       = "general-purpose"
-    size       = "s-1vcpu-2gb"
-    auto_scale = true
-    min_nodes  = 1
-    max_nodes  = 3
-  }
 }
+
+resource "digitalocean_kubernetes_node_pool" "general-purpose" {
+  cluster_id = digitalocean_kubernetes_cluster.sfo2.id
+  name       = "general-purpose"
+  size       = "s-1vcpu-2gb"
+  auto_scale = true
+  min_nodes  = 0
+  max_nodes  = 3
+}
+
