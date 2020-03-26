1add801f247a3c1bf906199bf8db064922d1c04e
Jono Cooper <jono@jonocooper.com>
2020-03-26 23:13:43 +1300

    Pin Kubernetes Provider

--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -1,6 +1,7 @@
 provider "kubernetes" {
-  host  = digitalocean_kubernetes_cluster.sfo2.endpoint
-  token = digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
+  version = "= 1.10.0"
+  host    = digitalocean_kubernetes_cluster.sfo2.endpoint
+  token   = digitalocean_kubernetes_cluster.sfo2.kube_config[0].token
   cluster_ca_certificate = base64decode(
     digitalocean_kubernetes_cluster.sfo2.kube_config[0].cluster_ca_certificate
   )
