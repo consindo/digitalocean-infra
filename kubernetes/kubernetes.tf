2c651b2752d89567f8d61c2013ba4ce55040a2b5
Jono Cooper <jono@jonocooper.com>
2021-10-17 12:01:04 +1300

    Update kubernetes.tf

--- kubernetes/kubernetes.tf
+++ kubernetes/kubernetes.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "sfo2" {
   name    = "sfo2-prod"
   region  = "sfo2"
-  version = "1.19.13-do.0"
+  version = "1.16.15-do.4"
 
   node_pool {
     name       = "general-purpose"
@@ -10,23 +10,5 @@ resource "digitalocean_kubernetes_cluster" "sfo2" {
     min_nodes  = 1
     max_nodes  = 3
   }
-}
-
-resource "digitalocean_kubernetes_node_pool" "memory-optimized" {
-  cluster_id = digitalocean_kubernetes_cluster.sfo2.id
-  name       = "memory-optimized"
-  size       = "s-1vcpu-3gb"
-  auto_scale = true
-  min_nodes  = 0
-  max_nodes  = 3
-}
-
-resource "digitalocean_kubernetes_node_pool" "compute-optimized" {
-  cluster_id = digitalocean_kubernetes_cluster.sfo2.id
-  name       = "compute-optimized"
-  size       = "s-2vcpu-2gb"
-  auto_scale = true
-  min_nodes  = 0
-  max_nodes  = 3
 }
 
